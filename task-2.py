b2cbc90e30b7db538d2aa022920bf5aa0902889d
KazakovK <zenitovec163@mail.ru>
2020-12-16 22:15:39 +0400

    Add task 2

---
+++ task-2.py
@@ -0,0 +1,8 @@
+def func_person(name, surname, year, city, mail, phone):
+    print(f'Имя сотрудника: {name}, Фамилия: {surname}, Год рождения: {year}, '
+          f'Город: {city}, Почта: {mail}, Телефон: {phone}')
+
+
+a, b, c, d, e, f = input('Введите через запятую: Имя,Фамилию, Год рождения,Город,E-mail,Телефон: ').split(',')
+
+func_person(surname=b, year=c, name=a, city=d, mail=e, phone=f)
