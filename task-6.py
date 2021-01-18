24440a11dcd635bf506d95d6b0620bb312496943
KazakovK <zenitovec163@mail.ru>
2021-01-19 00:17:50 +0400

    Add task 6

---
+++ task-6.py
@@ -0,0 +1,70 @@
+equipment = {}
+
+
+class Warehouse:
+    def __init__(self):
+        self.length = 10000
+        self.width = 15000
+        self.height = 50
+
+
+class Equipment:
+    def __init__(self, id_eq, price, name, quantity, subdivision):
+        self.id_eq = id_eq
+        self.price = price
+        self.name = name
+        self.quantity = quantity
+        self.subdivision = subdivision
+
+    def add_equipment(self):
+        other = [self.name, self.price, self.quantity, self.subdivision]
+        other_update = {self.id_eq: other}
+        equipment.update(other_update)
+
+    def validation(self):
+        try:
+            int(self.quantity)
+        except ValueError:
+            print("Количество техники должно быть числом!")
+
+
+class Printer(Equipment):
+    def __init__(self, id_eq, price, name, quantity, subdivision, print_speed):
+        super().__init__(id_eq, price, name, quantity, subdivision)
+        self.print_speed = print_speed
+
+    def add_equipment(self):
+        other = [self.name, self.price, self.quantity, self.subdivision, self.print_speed]
+        other_update = {self.id_eq: other}
+        equipment.update(other_update)
+
+
+class Scanner(Equipment):
+    def __init__(self, id_eq, price, name, quantity, subdivision, type_scan):
+        super().__init__(id_eq, price, name, quantity, subdivision)
+        self.type_scan = type_scan
+
+    def add_equipment(self):
+        other = [self.name, self.price, self.quantity, self.subdivision, self.type_scan]
+        other_update = {self.id_eq: other}
+        equipment.update(other_update)
+
+
+class Xerox(Equipment):
+    def __init__(self, id_eq, price, name, quantity, subdivision, performance):
+        super().__init__(id_eq, price, name, quantity, subdivision)
+        self.performance = performance
+
+    def add_equipment(self):
+        other = [self.name, self.price, self.quantity, self.subdivision, self.performance]
+        other_update = {self.id_eq: other}
+        equipment.update(other_update)
+
+
+a = Equipment(1, 12.3, "HP", 7, "Маркетинг")
+a.add_equipment()
+b = Equipment(2, 13.10, "Apple", "dsds", "Фин.отдел")
+b.validation()
+b.add_equipment()
+
+print(equipment)
